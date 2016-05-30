6ad2398838ea6e10a27b714f4ea2766e7e6dc386
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-05-30 15:53:25 +0200

    Creating board array

--- js/autoplay.js
+++ js/autoplay.js
@@ -2,7 +2,11 @@ var Total_secs;
 var Total_mins;
 var cronometer;
 
+var board = new Aray(8);
+
 function autoplay(){
+
+	for (i=0; i<8; i++) board[i] = new Array(8);
 
 	ResetTime();
 	StartTime();
