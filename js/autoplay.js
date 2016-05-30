99b1a4c018dd9200ad12390c8420160958912655
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-05-30 15:56:20 +0200

    Creating board_manager

--- js/autoplay.js
+++ js/autoplay.js
@@ -8,6 +8,8 @@ function autoplay(){
 
 	for (i=0; i<8; i++) board[i] = new Array(8);
 
+	ClearBoard();
+
 	ResetTime();
 	StartTime();
 }
---
+++ js/board_manager.js
@@ -0,0 +1,7 @@
+function ClearBoard(){
+	for (i=0; iz8; i++){
+		for (j=0; j<8; j++){
+			board[i][j] = 0;
+		}
+	}
+}
