cb402b3563796b1b9f379a262c4430a5f3f2a8c2
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-06-01 12:34:46 +0200

    Checking position to move

--- js/autoplay.js
+++ js/autoplay.js
@@ -19,6 +19,19 @@ function SelectCell(x, y){
 function CheckCell(x, y){
 	CheckTrue = false;
 
+	dif_x = x - CellSelected_x;
+	dif_y = y - CellSelected_y;
+
+	if (dif_x == 1 && dif_y == 2) CheckTrue = true;		// right - top long
+	if (dif_x == 2 && dif_y == 1) CheckTrue = true;		// right long - top
+	if (dif_x == 1 && dif_y == -2) CheckTrue = true;	// right - bottom long
+	if (dif_x == 2 && dif_y == -1) CheckTrue = true;	// right long - bottom
+
+	if (dif_x == -1 && dif_y == 2) CheckTrue = true;	// left - top long
+	if (dif_x == -2 && dif_y == 1) CheckTrue = true;	// left long - top
+	if (dif_x == -1 && dif_y == -2) CheckTrue = true;	// left - bottom long
+	if (dif_x == -2 && dif_y == -1) CheckTrue = true;	// left long - bottom
+
 	if (CheckTrue) SelectCell(x, y);
 }
 
