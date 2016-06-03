a107df44bf0ad52a1e92e259555571aa835fa086
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-06-03 13:43:29 +0200

    Checking and using Bonus

--- js/autoplay.js
+++ js/autoplay.js
@@ -11,6 +11,7 @@ var board = new Array(8);
 
 var CellSelected_x;
 var CellSelected_y;
+var CheckCell_Required;
 
 function Check_SuccessfullEnd(){
 	SuccessfullEnd = true;
@@ -32,7 +33,10 @@ function Check_GameOver(x, y){
 	Check_Moves(x, y, -2, -1);	// Check move left long - bottom
 
 	document.getElementById("options").innerHTML = Options;
-	if (!Options) alert("Game Over");
+	if (!Options){
+		if (Bonus) CheckCell_Required = false
+		else alert("Game Over");
+	}
 }
 
 function Check_Moves(x, y, mov_x, mov_y){
@@ -40,7 +44,7 @@ function Check_Moves(x, y, mov_x, mov_y){
 	option_y = y + mov_y;
 
 	if (option_x < 8 && option_y < 8 && option_x >= 0 && option_y >= 0){
-		if (board[option_x][option_y] == 0) Options++;
+		if (board[option_x][option_y] == 0 || board[option_x][option_y] == 2) Options++;
 	}
 }
 
@@ -61,6 +65,7 @@ function SelectCell(x, y){
 	CellSelected_x = x;
 	CellSelected_y = y;
 
+	CheckCell_Required = true;
 	Check_SuccessfullEnd();
 	Check_GameOver(x, y);
 	Check_newBonus();
@@ -88,20 +93,30 @@ function Check_newBonus(){
 }
 
 function CheckCell(x, y){
-	CheckTrue = false;
+	CheckTrue = true;
+	if (CheckCell_Required){
+		CheckTrue = false;
 
-	dif_x = x - CellSelected_x;
-	dif_y = y - CellSelected_y;
+		dif_x = x - CellSelected_x;
+		dif_y = y - CellSelected_y;
 
-	if (dif_x == 1 && dif_y == 2) CheckTrue = true;		// right - top long
-	if (dif_x == 2 && dif_y == 1) CheckTrue = true;		// right long - top
-	if (dif_x == 1 && dif_y == -2) CheckTrue = true;	// right - bottom long
-	if (dif_x == 2 && dif_y == -1) CheckTrue = true;	// right long - bottom
+		if (dif_x == 1 && dif_y == 2) CheckTrue = true;		// right - top long
+		if (dif_x == 2 && dif_y == 1) CheckTrue = true;		// right long - top
+		if (dif_x == 1 && dif_y == -2) CheckTrue = true;	// right - bottom long
+		if (dif_x == 2 && dif_y == -1) CheckTrue = true;	// right long - bottom
 
-	if (dif_x == -1 && dif_y == 2) CheckTrue = true;	// left - top long
-	if (dif_x == -2 && dif_y == 1) CheckTrue = true;	// left long - top
-	if (dif_x == -1 && dif_y == -2) CheckTrue = true;	// left - bottom long
-	if (dif_x == -2 && dif_y == -1) CheckTrue = true;	// left long - bottom
+		if (dif_x == -1 && dif_y == 2) CheckTrue = true;	// left - top long
+		if (dif_x == -2 && dif_y == 1) CheckTrue = true;	// left long - top
+		if (dif_x == -1 && dif_y == -2) CheckTrue = true;	// left - bottom long
+		if (dif_x == -2 && dif_y == -1) CheckTrue = true;	// left long - bottom
+	}
+	else{
+		if (board[x][y] == 0 || board[x][y] == 2){
+			Bonus--;
+			document.getElementById("bonus").innerHTML = "+" + Bonus;
+			if (Bonus == 0) document.getElementById("bonus").innerHTML = "";
+		}
+	}
 
 	if (board[x][y] == 1) CheckTrue = false;
 	if (CheckTrue) SelectCell(x, y);
