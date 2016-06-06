9bac11c914bbceea9b098e7d0fb416d5620719c8
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-06-06 10:30:51 +0200

    setLifes depending on Level

--- js/start_manager.js
+++ js/start_manager.js
@@ -1,7 +1,18 @@
+function setLifes(){
+	if (Level == 1) Lifes = 1;
+	if (Level == 2) Lifes = 4;
+	if (Level == 3) Lifes = 3;
+	if (Level == 4) Lifes = 3;
+	if (Level == 5) Lifes = 4;
+	if (Level == 6) Lifes = 3;
+	if (Level == 7) Lifes = 5;
+}
+
 function setLevel_Parameters(){
 
 	Moves = 64;
 	Moves_Required = 8;
 	Bonus = 0;
+	document.getElementById("bonus").innerHTML = "";
 
 }
