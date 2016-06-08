a4e92a7b571efa17b0e97d4be777e3e70aa8d37a
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-06-08 13:48:50 +0200

    Level 4, Level_Moves

--- js/start_manager.js
+++ js/start_manager.js
@@ -12,7 +12,7 @@ function set_LevelMoves(){
 	if (Level == 1) Level_Moves = 64;
 	if (Level == 2) Level_Moves = 56;
 	if (Level == 3) Level_Moves = 32;
-	if (Level == 4) Level_Moves = 3;
+	if (Level == 4) Level_Moves = 16;
 	if (Level == 5) Level_Moves = 4;
 	if (Level == 6) Level_Moves = 3;
 	if (Level == 7) Level_Moves = 5;
