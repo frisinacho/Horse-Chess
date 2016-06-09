0c22c0909d0c0e72b0f28e9fabfbd0a5d06ddee2
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-06-09 11:43:43 +0200

    Showing message notification

--- js/message_manager.js
+++ js/message_manager.js
@@ -3,4 +3,7 @@ function ShowMessage(string_notification, game_over){
 
 	Message = document.getElementById("message");
 	Message.style.display = "block";
+
+	Message_Notification = document.getElementById("notification");
+	Message_Notification.innerHTML = string_notification;
 }
