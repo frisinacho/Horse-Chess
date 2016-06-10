114bf91dab467e64002370a10be18c6c16fd183b
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-06-10 12:59:36 +0200

    Showing Score and Time in the final message

--- js/message_manager.js
+++ js/message_manager.js
@@ -1,9 +1,24 @@
 function ShowMessage(string_notification, game_over){
 	Next_Level = !game_over;
 
+	if (game_over == false){
+		string_score = "Level " + Level + " | Time ";
+		if (Total_mins < 10) string_score = string_score + "0";
+		string_score = string_score + Total_mins + ":";
+		if (Total_secs < 10) string_score = string_score + "0";
+		string_score = string_score + Total_secs;
+
+	}
+	else{
+
+	}
+
 	Message = document.getElementById("message");
 	Message.style.display = "block";
 
 	Message_Notification = document.getElementById("notification");
 	Message_Notification.innerHTML = string_notification;
+
+	Message_Notification = document.getElementById("data_message");
+	Message_Notification.innerHTML = string_score;
 }
